f8ffe6bfccb032be7eed670e6eecff6210fb88a8
DVORAK <aleksey.altaev@mail.ru>
2025-07-09 21:22:02 +0600

    feat: login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,41 @@
-'use client'
+"use client";
+import { useUserStore } from "@/store/user";
 import axios from "axios";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const router = useRouter();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async () => {
-    axios.post('https://dummyjson.com/auth/login', {
-    username: username,
-    password: password
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    try {
+      const response = await axios.post("https://dummyjson.com/auth/login", {
+        username: username,
+        password: password,
+      });
+      console.log(response);
+
+      document.cookie = `accessToken=${response.data.token}; path=/; secure; samesite=strict`;
+      console.log("its in cookies" + document.cookie);
+
+      useUserStore.getState().login({
+        accessToken: response.data.accessToken,
+        user: {
+          firstName: response.data.firstName,
+          lastName: response.data.lastName,
+          email: response.data.email,
+        },
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <>
       <div>LoginPage</div>
