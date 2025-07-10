239dcf9cb91d012d353f9d8aa6aa3958bffffc6c
DVORAK <aleksey.altaev@mail.ru>
2025-07-10 18:27:49 +0600

    fix: fix wrong description

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const loginSchema = z.object({
   username: z.string().min(3, 'Username must be at least 3 characters'),
-  password: z.string().min(3, 'Password must be at least 6 characters'),
+  password: z.string().min(3, 'Password must be at least 3 characters'),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
