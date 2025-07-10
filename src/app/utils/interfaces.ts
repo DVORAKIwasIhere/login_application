ca36b2a373bdd154a129a47a0350b81d3ee14bf0
DVORAK <aleksey.altaev@mail.ru>
2025-07-10 18:03:19 +0600

    chore: adjusting product interface

--- src/app/utils/interfaces.ts
+++ src/app/utils/interfaces.ts
@@ -1,9 +1,11 @@
 export interface Product {
   id: number;
   title: string;
+  category: string;
+  price: number;
+  thumbnail: string;
 }
 
 export interface ProductProps {
-    products: Product
-    
+  products: Product;
 }
