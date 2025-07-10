02069f21c9de9890d845868009a29b5e24621c36
DVORAK <aleksey.altaev@mail.ru>
2025-07-10 18:04:21 +0600

    chore: header adjustment + style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+"use client";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+import Link from "next/link";
+import { useUserStore } from "@/store/user";
 
+import styles from "./Header.module.scss";
 
-export default Header;
+export default function Header() {
+  const accessToken = useUserStore((s) => s.accessToken);
+  const logout = useUserStore((s) => s.logout);
+
+  return (
+    <header>
+      <div className={styles.header}>
+        <div className={styles.contacts}>
+          <span>📞 +021-95-51-84</span>
+          <span>✉️ shop@abelohost.com</span>
+          <span>🏠 1734 Stonecoal Road</span>
+        </div>
+        <div>
+          {accessToken ? (
+            <button onClick={logout} className={styles.logoutButton}>
+              🔓 Logout
+            </button>
+          ) : (
+            <Link href="/login" className={styles.loginButton}>
+              👤 Login
+            </Link>
+          )}
+        </div>
+      </div>
+        <div className={styles.banner}>
+          <h1 className={styles.title}>Abelohost Shop.</h1>
+          <div className={styles.bannerLogo}></div>
+          {/* placeholder */}
+        </div>
+    </header>
+  );
+}
