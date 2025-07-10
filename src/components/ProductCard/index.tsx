37b9e0b99d42d716c2258d933774a571723fd136
DVORAK <aleksey.altaev@mail.ru>
2025-07-10 18:04:45 +0600

    feat: product styles

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,10 +1,23 @@
-import { ProductProps } from '@/app/utils/interfaces'
-import React from 'react'
+import { ProductProps } from "@/app/utils/interfaces";
+import React from "react";
+import styles from "./ProductCard.module.scss";
+import { useUserStore } from "@/store/user";
 
- const ProductCard = ({products}: ProductProps) => {
+const ProductCard = ({ products }: ProductProps) => {
+  const accessToken = useUserStore((s) => s.accessToken);
   return (
-    <div>{products.title}</div>
-  )
-}
+    <div className={styles.card}>
+      <img
+        src={products.thumbnail}
+        alt={products.title}
+        className={styles.image}
+      />
+      <div className={styles.title}>{products.title}</div>
+      <div className={styles.category}>{products.category}</div>
+      <div className={styles.price}>${products.price}</div>
+      {accessToken && <button className={styles.button}>add to cart</button>}
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
